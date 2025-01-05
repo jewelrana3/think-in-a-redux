0b8d8b1e36e1152c7db93f185ea5ff96004c5f21
jewelrana3 <juyelrana7752@gmail.com>
2025-01-05 17:56:03 +0600

    action

---
+++ src/redux/filters/action.js
@@ -0,0 +1,14 @@
+import { COLORCHANGED, STATUSCHANGED } from "./actionTypes";
+
+export const statusChange = (status) => {
+  return {
+    type: STATUSCHANGED,
+    payload: status,
+  };
+};
+export const colorChange = (colorId) => {
+  return {
+    type: COLORCHANGED,
+    payload: colorId,
+  };
+};
