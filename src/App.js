6865b2fce198bd9c1acd7c343dc20c169f2fe422
jewelrana3 <juyelrana7752@gmail.com>
2025-02-14 16:05:43 +0600

    routing setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from "react";
 
 import Video from "./pages/Video";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div>
-      <Video />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
